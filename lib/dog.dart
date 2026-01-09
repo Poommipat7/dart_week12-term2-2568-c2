3a12c333ed395e1a08a81fbe054e726369bda91a
Poommipat7 <poommipat462@gmail.com>
2026-01-09 12:49:21 +0700

    feature/inheritance

--- lib/dog.dart
+++ lib/dog.dart
@@ -1,18 +1,22 @@
-class Dog {
   final String? name;
   final int? age;
   final int? dogId;
+  String? breed;
 
+  //2.Constant constructor
   Dog({this.name, this.age, this.dogId});
+  Dog({this.name, this.age, this.dogId, this.breed});
 
   void dogInfo() {
     print("Dog of Name: $name");
     print("Dog of age: $age");
     print("Dog of dogId: $dogId");
+    print("Dog of dogId: $breed");
   }
 }
 
 void callDog() {
   final Dog dog = Dog(name: "Thong", age: 20, dogId: 101);
+  final Dog dog = Dog(name: "Thong", age: 20, dogId: 101, breed: "Blue dog");
   dog.dogInfo();
 }
